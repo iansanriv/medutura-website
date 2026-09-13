0779ec43b9e49bba8e1973eb2ed1af6b125fa53d
Ian Sánchez <sanchezriveraian@gmail.com>
2026-09-13 02:54:39 -0400

    Delete drizzle directory

--- drizzle/0001_wandering_warpath.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX `idx_order_items_order_id` ON `order_items` (`order_id`);--> statement-breakpoint
-CREATE INDEX `idx_orders_created_at` ON `orders` (`created_at`);--> statement-breakpoint
-CREATE INDEX `idx_orders_processor_reference` ON `orders` (`processor_reference`);--> statement-breakpoint
-CREATE INDEX `idx_products_active_featured_created` ON `products` (`active`,`featured`,`created_at`);--> statement-breakpoint
-PRAGMA optimize;
